16c2e486ec6adc65b7aa31597155e5cf2585c184
Eugene.Sinuk <eugene.sinuk@nordigy.ru>
2021-04-29 17:28:49 +0300

    try-out-fixes

--- src/main/java/com/andersen/siniuk/ui/CalculatorPage.java
+++ src/main/java/com/andersen/siniuk/ui/CalculatorPage.java
@@ -28,23 +28,11 @@ public class CalculatorPage extends BasePage {
     @FindBy(xpath = "//input[@data-ng-model='currencyExchangeVM.filter.from_amount']")
     private static WebElement sellInput;
 
-    @FindBy(xpath = "(//span[contains(@class,'flag-icon-us')]/../following-sibling::*)[3]")
-    private WebElement usdCompanyAmount;
-
-    @FindBy(xpath = "(//span[contains(@class,'flag-icon-us')]/../following-sibling::*)[4]//span[@class='ng-binding']")
-    private WebElement usdSwedBankAmount;
-
-    @FindBy(xpath = "(//span[contains(@class,'flag-icon-us')]/../following-sibling::*)[4]//span[starts-with(@class, 'other-bank-loss')]")
-    private WebElement swedBankLoss;
-
     @FindBy(css = ".js-localization-popover")
     private WebElement localizationPopover;
 
     @FindBy(id = "countries-dropdown")
     private WebElement countriesDropdown;
-
-    @FindBy(xpath = "(//span[contains(@class,'flag-icon-us')]/../following-sibling::*)[1]")
-    private WebElement usdOfficialRate;
 
     @FindBy(xpath = "//div[contains(@class, 'ui-select-match')]")
     private WebElement sellCurrency;
@@ -77,30 +65,6 @@ public class CalculatorPage extends BasePage {
         sellInput.clear();
         sellInput.sendKeys(value);
         return this;
-    }
-
-    @Step("Get USD Company amount")
-    public BigDecimal getUSDCompanyAmount() {
-        Actions.scrollIntoView(usdCompanyAmount);
-        return BigDecimal.valueOf(Double.parseDouble(usdCompanyAmount.getText()));
-    }
-
-    @Step("Get USD SwedBank amount")
-    public BigDecimal getUSDSwedBankAmount() {
-        Actions.scrollIntoView(usdSwedBankAmount);
-        return BigDecimal.valueOf(Double.parseDouble(usdSwedBankAmount.getText()));
-    }
-
-    @Step("Get SwedBank difference")
-    public BigDecimal getCompanySwedBankLoss() {
-        Actions.scrollIntoView(swedBankLoss);
-        return BigDecimal.valueOf(Double.parseDouble(swedBankLoss.getText().replaceAll("[()^]*", "")));
-    }
-
-    @Step("Get USD Official rate")
-    public String getUSDOfficialRate() {
-        Actions.scrollIntoView(usdOfficialRate);
-        return usdOfficialRate.getText();
     }
 
     @Step("Get SELL currency")
